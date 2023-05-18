cab7a3b9af91cd5b4ffdc5e917c81c257aa4b5f2
LHENI <lheni644@gmail.com>
2023-05-18 14:10:16 +0200

    first commit

--- test/app.spec.js
+++ test/app.spec.js
@@ -16,14 +16,14 @@ describe('Sikertelen versenyzők számának számítása', () => {
 describe('Sikertelen versenyzők százalékának számítása', () => {
     it('Versenyzők:100, Célbaértek:10 ',()=>{
         let act = getUnsuccessPercent(100,10);
-        expect(act).toBe(10);
+        expect(act).toBe(90);
     });
     it('Versenyzők:200, Célbaértek:10 ',()=>{
         let act = getUnsuccessPercent(200,10);
-        expect(act).toBe(5);
+        expect(act).toBe(95);
     });
     it('Versenyzők:500, Célbaértek:50 ',()=>{
         let act = getUnsuccessPercent(500,50);
-        expect(act).toBe(10);
+        expect(act).toBe(90);
     });
 });
